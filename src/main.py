1ceb51a3dc277b30559d953b927e76140078b404
Sreetx <98646763+Sreetx@users.noreply.github.com>
2025-02-26 20:46:59 +0700

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -51,17 +51,40 @@ try:
         from PIL import ImageEnhance
         import numpy as np
         import io
+        from pathlib import Path
     except Exception as e:
         print(e)
         print(" [!] Coba install module ini: pillow, numpy, rembg");sys.exit()
 except KeyboardInterrupt: print(" [!] Dibatalkan...");sys.exit()
 
-def image_eraser(media, output, model):
+os.system('cls || clear')
+def image_eraser(media, output, model, url):
     if model is None or model == "":
         print(kelabu+" ["+banmerah+"!"+reset+kelabu+"]"+putih+" Masukkan model untuk pemrosesan!"+reset);sys.exit()
     default_path = media.split("/")[-1]
     if not os.path.exists('hasil'):
         os.makedirs('hasil')
+    if url:
+        if not os.path.exists('cache'):
+            os.makedirs('cache')
+        try:
+            import socket, requests
+        except ImportError: print(kelabu+" ["+banorange+"!"+reset+kelabu+"]"+putih+" Module Requests tidak ditemukan, silakan install: pip install requests");sys.exit()
+        socket.create_connection(('8.8.8.8', 53), timeout=3)
+        print(kelabu+" ["+orange+"*"+reset+kelabu+"]"+putih+" Mengunduh gambar..."+reset)
+        req = requests.get(url)
+        dirr = url.split("/")[-1]
+        caches = os.path.join('cache', dirr)
+        with open (caches, 'wb') as ca:
+            ca.write(req.content)
+    if media == "":
+        media = 'cache/'+dirr
+    if default_path == "":
+        default_path = dirr
+    else:
+        pass
+    print(kelabu+" ["+banorange+"!"+reset+kelabu+"]"+putih+" Memproses...."+reset)
+
     if output == '':
         with open(media, 'rb') as imag:
             imaga = imag.read()
@@ -95,36 +118,29 @@ def image_eraser(media, output, model):
         print(putih+" ["+banorange+"✔️"+reset+putih+"] Pemrosesan gambar Berhasil!"+reset)
         print(putih+" ["+banhijau+"#"+reset+putih+"] Gambar disimpan di "+output+"eraser-"+default_path+reset);sys.exit()
 
-def help():
+def help(hh):
     banner()
-    print("""
-Options: python3 main.py [option]
-    --mode      Buat milih mode "image-background-eraser" atau "video-background-eraser"
-    --media     Letakan path media seperti gambar atau video pake opsi ini
-    --easy-mode Pake easy mode langsung kalo nggak terbiasa dengan option parser
-    --hh        Tampilkan menu bantuan ini!
-    -o --output Tentukan lokasi gambar disimpan (default: "hasil")
-    --model     Masukkan model yang akan di gunakan ('u2net'; 'u2net_human_seg'; 'silueta'; 'inset-general-use';)
-    --update
-    --update-all
-Usage:
-    python3 main.py --mode=image-background-eraser --model=u2net --media="/path/to/you image" -o /folder/destination/
-    python3 main.py --mode=video-background-eraser --model=u2net --media=/path/to/video -o /folder/destination/
-    python3 main.py --easy-mode
-    python3 main.py --hh
+    if hh == 'indonesia':
+        with open("color/tutorial-id.txt", 'r') as tut:
+            c = tut.read()
+        print(c);sys.exit()
+    if hh == 'english':
+        with open("color/tutorial-en.txt", 'r') as tut:
+            c = tut.read()
+        print(c);sys.exit()
+    else:
+        print(putih+' ['+banmerah+'!'+reset+putih+'] Mohon masukkan input! --hh');sys.exit()
 
- """+kelabu+"""["""+orange+"""*"""+kelabu+"""]"""+putih+""" Tanpa menggunakan opsi output. file akan otomatis di simpan di Eraser/src/hasil/
- """+kelabu+"""["""+orange+"""*"""+kelabu+"""]"""+putih+""" Wajib menggunakan backslash untuk memasukkan path media atau path output!"""+reset)
-    sys.exit()
 #Menggunakan ArgumentParser karena jika penggunakan OptionParser input spasi tidak dapat ditangkap dengan benar
 menu = ArgumentParser()
-menu.add_argument('--mode', dest='mode', help='Tentukan mode')
-menu.add_argument('--media', dest='media', help='Masukkan media gambar atau video yang akan di bersihkan background nya')
+menu.add_argument('--mode', dest='mode', default='', help='Tentukan mode')
+menu.add_argument('--media', dest='media',default='', help='Masukkan media gambar atau video yang akan di bersihkan background nya')
 menu.add_argument('--easy-mode', dest='easy_mode', action="store_true", default=False, help='Masuk ke easy mode aja kalo mode option parser terasa sulit')
-menu.add_argument('--hh', dest='hh', action='store_true', default=False, help='Menu bantuan')
+menu.add_argument('--hh', dest='hh', help='Menu bantuan')
 menu.add_argument('-o', '--output', dest='output', default='', help='Lokasi file akan disimpan')
 menu.add_argument('--update', dest='update', action='store_true', default=False, help='Hanya mengupdate script utama')
 menu.add_argument('--update-all', dest='updated', action='store_true', default=False, help='Untuk mengupdate semua module script bahkan mengupdate script utama')
+menu.add_argument('--url', dest='url', default='', help='Unduh dan hapus latar belakang gambar secara langsung!')
 menu.add_argument('--model', dest='model', help='Pilih Model untuk proses')
 option = menu.parse_args()
 
@@ -136,20 +152,20 @@ output = str(option.output).strip()
 update = option.update
 update_all = option.updated
 model = option.model
+url = option.url
 
 if mode == "image-background-eraser":
     bannerd()
     print(kelabu+" ["+banhijau+"#"+reset+kelabu+"]"+putih+" Mode Image Background Eraser...."+reset)
     print(kelabu+" ["+banhijau+"!"+reset+kelabu+"]"+putih+" Mungkin proses akan sedikit lambat tergantung deivce"+reset)
-    print(kelabu+" ["+banorange+"!"+reset+kelabu+"]"+putih+" Memproses...."+reset)
-    image_eraser(media, output, model)
+    image_eraser(media, output, model, url)
 if mode == "video-background-eraser":
     bannerd()
     print(kelabu+" ["+banhijau+"#"+reset+kelabu+"]"+putih+" Mode Video Background Eraser...."+reset)
     print(putih+" ["+banmerah+"*"+reset+putih+"] Coming Soon!!..."+reset);sys.exit()
     print(putih+" ["+banmerah+"*"+reset+putih+"] Nantikan Update berikutnya!..."+reset);sys.exit()
 if hh:
-    help()
+    help(hh)
 if update_all:
     try:
         import socket, requests
@@ -159,7 +175,11 @@ if update_all:
     maind = requests.get("https://raw.githubusercontent.com/Sreetx/Eraser/refs/heads/master/src/main.py")
     colorr = requests.get("https://raw.githubusercontent.com/Sreetx/Eraser/refs/heads/master/src/color/warna.py")
     logoo = requests.get("https://raw.githubusercontent.com/Sreetx/Eraser/refs/heads/master/src/color/logo.txt")
+    tutor_id = requests.get("https://raw.githubusercontent.com/Sreetx/Eraser/refs/heads/master/src/color/tutorial-id.txt")
+    tutor_en = requests.get("https://raw.githubusercontent.com/Sreetx/Eraser/refs/heads/master/src/color/tutorial-id.txt")
     maind_byte = maind.content.decode("utf-8")
+    tutor_id_byte = tutor_id.content.decode("utf-8")
+    tutor_en_byte = tutor_en.content.decode("utf-8")
     colorr_byte = colorr.content.decode("utf-8")
     logoo_byte = logoo.content.decode("utf-8")
     os.makedirs('color', exist_ok=True)
@@ -172,6 +192,11 @@ if update_all:
     print(kelabu+" ["+banorange+"UPDATE"+reset+kelabu+"]"+putih+" Updating Logo/Banner..."+reset);time.sleep(0.2)
     with open("color/logo.txt", "w", encoding="utf-8") as c:
         c.write(logoo_byte)
+    print(kelabu+" ["+banorange+"UPDATE"+reset+kelabu+"]"+putih+" Installing Guide text..."+reset);time.sleep(0.2)
+    with open("color/tutorial-id.txt", "w", encoding="utf-8") as d:
+        d.write(tutor_id_byte)
+    with open("color/tutorial-id.txt", "w", encoding="utf-8") as e:
+        e.write(tutor_en_byte)
     print(putih+" ["+banhijau+"UPDATE"+reset+putih+"] Update Succed!"+reset);sys.exit()
 
 if update:
@@ -189,16 +214,21 @@ if easy_mode:
     print(kelabu+" ["+banhijau+"#"+reset+kelabu+"]"+putih+" Easy Mode...."+reset)
     mode = input(kelabu+" ["+orange+">"+kelabu+"]"+putih+" Pilih Mode, [image-background-eraser] or [video-background-eraser]: ")
     if mode == "image-background-eraser":
-        media = str(input(kelabu+" ["+orange+">"+reset+kelabu+"]"+putih+" Masukan path file gambar (.jpg; .png; .jpeg;): "+reset).strip())
+        media = str(input(kelabu+" ["+orange+">"+reset+kelabu+"]"+putih+" Masukan path file gambar [lewatkan jika menggunakan link(enter)] (.jpg; .png; .jpeg;): "+reset).strip())
+        if media == "":
+            url = str(input(kelabu+" ["+orange+">"+reset+kelabu+"]"+putih+" Masukkan link gambar: "+reset).strip())
         output = str(input(kelabu+" ["+orange+">"+reset+kelabu+"]"+putih+" Masukan path output: "+reset).strip())
-        model = str(input(kelabu+" ["+orange+">"+reset+kelabu+"]"+putih+" Masukan model ('u2net'; 'u2net_human_seg'; 'silueta'; 'inset-general-use'): "+reset).strip())
+        model = str(input(kelabu+" ["+orange+">"+reset+kelabu+"]"+putih+" Masukan model ('u2net'; 'u2net_human_seg'; 'silueta'; 'isnet-general-use'; isnet-anime', etc.): "+reset).strip())
         print(kelabu+" ["+banhijau+"!"+reset+kelabu+"]"+putih+" Mungkin proses akan sedikit lambat tergantung deivce"+reset)
-        print(kelabu+" ["+banorange+"!"+reset+kelabu+"]"+putih+" Memproses...."+reset)
-        image_eraser(media, output, model)
+        image_eraser(media, output, model, url)
     if mode == "video-background-eraser":
         bannerd()
         print(kelabu+" ["+banorange+"!"+reset+kelabu+"]"+putih+" Fitur belum tersedia, Nantikan update selanjutnya"+reset)
         pass
 else:
-    help()
-    print(kelabu+" ["+banorange+"!"+reset+kelabu+"]"+putih+" Input lu salah atau lu gk masukkan input sama sekali!")
+    print(" [*] Importing Module...")
+    print(" [*] Mungkin agak lama karena kami harus mengimpor file logo.txt ini juga berpengaruh tergantung kecepatan device kalian!")
+    banner()
+    print(kelabu+" ["+banorange+"!"+reset+kelabu+"]"+putih+" Wajib menggunakan BackSlash di semua sistem!")
+    print(kelabu+" ["+orange+"*"+reset+kelabu+"]"+putih+" coba ketik python3 main.py --hh indonesia | english");sys.exit()
+    print(kelabu+" ["+orange+"*"+reset+kelabu+"]"+putih+" Untuk panduan lengkap");sys.exit()
